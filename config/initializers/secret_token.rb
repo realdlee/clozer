f375fe3913300ce280c57fee8b2a6c89cc797d9f
David Lee <ambitiouspear@gmail.com>
2014-08-16 11:02:08 -0700

    Start new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Clozer::Application.config.secret_key_base = '7cf4267941bdb3a960a9be2554243a1cbcf5c11b5e456f8da553227c6cfbafb0bb926074fa0e9a764937fcc2475ef998c15478e83dcb2de70a9d32d6f93c33f0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Clozer::Application.config.session_store :cookie_store, key: '_clozer_session'
